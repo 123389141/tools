2ab9f809a152e851546bfbc073675caeb1a6bbae
王志鹏 <123389141@qq.com>
2014-12-16 14:23:14 +0800

    调整脚本

--- clear_logs.bat
+++ clear_logs.bat
@@ -1,12 +1,10 @@
 @echo off
 rem 删除windows指定目录下log文件，利用window定时任务每小时执行一次。
 
+echo %date% %time% >> C:\hotel_script\clear_logs.log
+
 set "dirc=D:\elong_log\log\rabbitmq_failed\"
 del /s %dirc%*.log
 
 set "dirc=D:\elong_log\log\SendLog\"
 del /s %dirc%*.log
-
-echo %date% %time% >> C:\hotel_script\clear_logs.log
-
-pause
